c68a05f383cc3d579a9ce2bbeea439c3e90bd0e0
root <olusanyaabdulrauf28@gmail.com>
2022-06-05 14:45:36 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * C program that prints exactly and that piece of art is useful
+ * Dora Korpar,2015-10-19, followed by a new line, to the standard error.
+ */
+int main(void)
+{
+	write("and that piece of art is useful" - Dora Korpar, 2015-10-19");
+	return (1);
+}
