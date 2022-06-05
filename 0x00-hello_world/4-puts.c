a22d2455ab483e82e453139fc8aea55a62a77e9b
root <olusanyaabdulrauf28@gmail.com>
2022-06-05 05:01:02 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
+/**
+ *
+ * Entry main point
+ *
+ * returns 0 (success)
+ *
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
 
 
