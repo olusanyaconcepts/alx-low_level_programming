40b41137f328d11b4940e1727e4b35d294ee8b28
root <olusanyaabdulrauf28@gmail.com>
2022-06-06 13:23:25 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
+ * main - Entry point
+ *
  * C program that prints exactly and that piece of art is useful
  * Dora Korpar,2015-10-19, followed by a new line, to the standard error.
+ *
+ * Return: Always 1 (Required)
  */
+
 int main(void)
 {
-write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
